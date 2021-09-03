4515e04b79b65f94dbdece6dd2be70a710023c2d
Wisam Naji <guitar90rocker@gmail.com>
2021-09-03 16:40:09 +0000

    redirects and tweaks

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,10 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { addItem, removeItem } from "../store/cart/cartSlice";
 import CartSummary from "../components/CartSummary";
 import CartItem from "../components/CartItem";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+
+  const isEmpty = cart.items.length === 0;
 
   return (
     <div className="mx-auto mt-4 md:mt-10 flex flex-wrap md:flex-nowrap md:flex-row-reverse justify-center px-2 lg:px-12 xl:px-24">
@@ -17,7 +20,17 @@ export default function Cart() {
       <div className="w-full md:w-4/6">
         <div className="md:sticky top-16 py-2 bg-white">
           <div className="bg-white">
-            <h2 className="text-2xl font-semibold py-3">Your Cart</h2>
+            <h2 className="text-2xl font-semibold py-3">
+              Your cart {isEmpty && "is empty."}
+            </h2>
+            {isEmpty && (
+              <Link
+                to="/"
+                className="text-red-600 hover:text-red-500 hover:underline"
+              >
+                Start shopping.
+              </Link>
+            )}
           </div>
         </div>
         <div className="divide-y">
